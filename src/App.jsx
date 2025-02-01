94a67f7f97afab1b0da33463ba69e96c80d494a0
Naveen S Bandara <sandaru.eng@gmail.com>
2025-02-01 10:53:05 +0530

    Initial development work

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-primary font-bold text-3xl ">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
